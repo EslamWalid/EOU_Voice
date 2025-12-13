a6016454d09de66b01de009b4df2f2a0f0a43998
EslamWalid <eslamwalid9000@gmail.com>
2025-12-14 01:36:39 +0200

    first commit

---
+++ arabic_eou/sdk.py
@@ -0,0 +1,18 @@
+
+from arabert.preprocess import ArabertPreprocessor
+
+class ArabicEOUDetector:
+    """
+    Wraps your Hugging Face Arabic EOU classifier
+    """
+    def __init__(self, model_name="EslamWalid/bert-classifier"):
+        from transformers import pipeline
+        self.classifier = pipeline(model=model_name)
+        self.preprocessor = ArabertPreprocessor("aubmindlab/bert-base-arabertv02")
+
+    def will_end_utterance(self, text: str) -> float:
+        text = self.preprocessor.preprocess(text)
+        result = self.classifier(text)
+        label, score = result[0]["label"], result[0]["score"]
+        probability = score if label.upper() == "EOU" else 1 - score
+        return float(probability)
